6d37b6adf488281807626ccfc61e65e8ee7e5c7b
Tú Đỗ <thanhtu19052000@gmail.com>
2025-05-18 15:47:22 +0700

    change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello, CI/CD with GitHub and ArgoCD!'));
+app.get('/', (req, res) => res.send('Hello, CI/CD with GitHub and ArgoCD! 2'));
 app.listen(3000, () => console.log('Server running on port 3000'));
